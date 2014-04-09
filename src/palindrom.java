c11b8afcb4ebd798181ea5d19379618d7a0534e9
Andrea Rojas <rojasmadero@gmail.com>
2014-04-10 00:27:03 +0200

    green: no_palindrom_returns_empty_output

--- src/palindrom.java
+++ src/palindrom.java
@@ -3,6 +3,15 @@ public class palindrom {
 	
 	public static String filter(String input){
 		String result = input;
+		int inputHalfLength = input.length()/2; //it's works for par and impar lenghts
+		int inputLength = input.length();
+		int i = 0;
+		
+		for(i=0;i<inputHalfLength;i++){
+			if (input.charAt(i) != input.charAt(inputLength-1-i)){
+				return "";
+			}
+		}
 		return result;
 	}
 
