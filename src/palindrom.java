29df8260a2a327df17f27ff7a9da1a1901cebb33
Andrea Rojas <rojasmadero@gmail.com>
2014-04-10 07:39:55 +0200

    green: two_palindrom_returns_two_palindrom

--- src/palindrom.java
+++ src/palindrom.java
@@ -1,7 +1,20 @@
+import java.util.Arrays;
+
 
 public class palindrom {
 	
 	public static String filter(String input){
+	    String[] inputInWords = input.split(" ");
+	    String[] outputInWords = new String[inputInWords.length];
+	    int i = 0;
+	    
+	    for (i=0;i<inputInWords.length;i++){
+	    	outputInWords[i] = checkPalindrom(inputInWords[i]);	    		    	    
+	    }
+	    return join(outputInWords, " ");
+	}
+
+	private static String checkPalindrom(String input) {
 		String result = input;
 		int inputHalfLength = input.length()/2; //it's works for par and impar lenghts
 		int inputLength = input.length();
@@ -14,5 +27,15 @@ public class palindrom {
 		}
 		return result;
 	}
-
+	
+	private static String join(String input[], String join) {
+    StringBuilder sb = new StringBuilder();
+    int i;
+    
+    for (i = 0; i < input.length - 1; i++) {
+    	sb.append(input[i] + join);
+    }      
+    return sb.toString() + input[i];
+    }
+	
 }
